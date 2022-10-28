c8db31996b95f8019fde40ece361992748ef9d8b
lorpeh <63863010+lorpeh@users.noreply.github.com>
2022-10-28 12:56:02 +0100

    Update views.py

--- HNGAPP/views.py
+++ HNGAPP/views.py
@@ -16,7 +16,7 @@ bio = "Hi,My name is Tolulope.I am an upcoming backend developer and i have a gr
 def get_Profile(request):
     profile = Profile.objects.all().first()
     app_url = ProfileSerializers(profile)
-    # Profile.objects.create(username=username, Age=Age, Bio=Bio)
+    # Profile.objects.create(slackUsername=slackusername, age=age, bio=bio)
     # serializer = ProfileSerializers(date=request.data)
     # serializer.is_valid(raise_exception=True)
     return Response(app_url.data, status=status.HTTP_200_OK)
